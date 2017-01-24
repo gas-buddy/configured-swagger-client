5e2b8ac9f76f7d6519bf341c0e4d92d637b74546
Max Metral <max@pyralis.com>
2017-01-24 09:17:55 -0500

    Fix tests to use a working petstore.io method

--- test/test_clients.js
+++ test/test_clients.js
@@ -7,23 +7,19 @@ tap.test('Client should work', async (t) => {
   services = await clientConfig.configureServices({
     pets: 'http://petstore.swagger.io/v2/swagger.json',
   });
-  const pets = await services.Pets.pet.findPetsByStatus({
-    status: 'pending',
-  });
+  const pets = await services.Pets.pet.getPetById({ petId: 1 });
   t.strictEquals(pets.status, 200, 'Should find pets');
   t.ok(pets.obj, 'Should return an obj');
-  t.end();
 });
 
 tap.test('Client should support interception', async (t) => {
   t.plan(1);
-  await services.Pets.pet.findPetsByStatus({ status: 'pending' }, {
+  await services.Pets.pet.getPetById({ petId: 1 }, {
     requestInterceptor: function intercept() {
       t.ok(true, 'Should call interceptor');
       return this;
     },
   });
-  t.end();
 });
 
 tap.test('Client should support proxy', async (t) => {
@@ -34,13 +30,8 @@ tap.test('Client should support proxy', async (t) => {
       return this;
     },
   });
-  const res = await proxied.Pets.pet.addPet({
-    body: {
-      name: 'doggie',
-    },
-  });
+  const res = await proxied.Pets.pet.getPetById({ petId: 1 });
   t.strictEquals(res.status, 200, 'Should return 200');
   t.ok(res.obj.id, 'Should have an id');
   t.ok(!proxied.Pets.pet.doesNotExist, 'Should return null for non-existent method');
-  t.end();
 });
